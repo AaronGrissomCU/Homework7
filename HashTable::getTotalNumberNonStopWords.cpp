73c448effe8ef752da513c74da9e420ca916ff34
AaronGrissomCU <32560635+AaronGrissomCU@users.noreply.github.com>
2017-11-05 16:30:33 -0700

    Update HashTable::getTotalNumberNonStopWords.cpp
    Passes 1st test, does not pass 2nd 3rd 4th tests
    1 - 11, 22, 7, 6, 20, 49
    2- 22, 22, 10, 10, 38
    3 & 4 - from file

--- HashTable::getTotalNumberNonStopWords.cpp
+++ HashTable::getTotalNumberNonStopWords.cpp
@@ -1,4 +1,18 @@
 int HashTable::getTotalNumberNonStopWords()
 {
-    // Your code here
+    wordItem* scout = nullptr;
+    int numNonStopWords = 0;
+    
+    for (int c = 0; c < STOPWORD_LIST_SIZE; c++)
+    {
+        scout = hashTable[c];
+        
+        while (scout != nullptr)
+        {
+            numNonStopWords += scout->count;
+            scout = scout->next;
+        }
+    }
+    
+    return numNonStopWords;
 }
