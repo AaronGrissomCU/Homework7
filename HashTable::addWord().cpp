3f670ad5e6e2a20a94da8fe05881de5e32205e81
AaronGrissomCU <32560635+AaronGrissomCU@users.noreply.github.com>
2017-11-04 19:55:51 -0600

    Update HashTable::addWord().cpp
    Passes test to update where it goes in the table, but does not add to hashtable

--- HashTable::addWord().cpp
+++ HashTable::addWord().cpp
@@ -1,4 +1,28 @@
 void HashTable::addWord(std::string word)
 {
-    // Your code here
+    bool isStopWord = false;
+    
+    for (int c = 0; c < STOPWORD_LIST_SIZE; c++)
+    {
+        if (vecIgnoreWords[c] == word)
+            isStopWord = true;
+    }
+    
+    if (!isStopWord)
+    {
+        int hashNum = getHash(word);
+        wordItem* wordPtr = nullptr;
+        wordPtr = searchTable(word);
+        
+        if (wordPtr != nullptr)
+        {
+            wordPtr->count ++;
+            numCollisions ++;
+        }
+        
+        else
+        {
+            wordItem* locator = hashTable[hashNum];
+        }
+    }
 }
