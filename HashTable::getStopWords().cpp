9837c6ff9341a53e1353e1dc89ab8490a72e3a88
AaronGrissomCU <32560635+AaronGrissomCU@users.noreply.github.com>
2017-11-04 19:20:16 -0600

    Update HashTable::getStopWords().cpp
    Passed all tests

--- HashTable::getStopWords().cpp
+++ HashTable::getStopWords().cpp
@@ -1,4 +1,15 @@
 void HashTable::getStopWords(char *ignoreWordFileName)
 {
-    // Your code here
+    ifstream input;
+    string text;
+    int counter = 0;
+    input.open(ignoreWordFileName);
+    
+    while (getline(input, text))
+    {
+        vecIgnoreWords[counter] = text;
+        counter ++;
+    }
+    
+    input.close();
 }
